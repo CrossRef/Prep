a68e70052e2453994fa89ca6258822064af71454
leoandplus <lfabrikant@andplus.com>
2018-02-14 11:19:04 -0500

    Prep-1 & Prep-6 search and search history complete

--- app/components/landingPage/__tests__/search.test.js
+++ app/components/landingPage/__tests__/search.test.js
@@ -5,20 +5,20 @@ Enzyme.configure({ adapter: new Adapter() });
 import toJson from 'enzyme-to-json';
 import sinon from 'sinon'
 
+import deployConfig from '../../../../deployConfig'
 import Search from '../components/search'
 
 
-let savedSearches = JSON.stringify([{name: 'saved search 1', id: 1}, {name: 'saved search 2', id: 2}])
-const parsedSavedSearches = JSON.parse(savedSearches)
+//Dummy Data
+const parsedSavedSearches = [
+  {name: 'saved search 21', id: 21},
+  {name: 'saved search 22', id: 22},
+  {name: 'saved search 23', id: 23},
+  {name: 'saved search 24', id: 24},
+  {name: 'saved search 25', id: 25},
+]
 
-
-const localStorageMock = {
-  getItem: sinon.stub().callsFake(() => savedSearches),
-  setItem: sinon.stub(),
-  clear: sinon.stub()
-};
-global.localStorage = localStorageMock;
-
+let savedSearches = JSON.stringify(parsedSavedSearches)
 
 const members = {
   'status': 'ok',
@@ -34,19 +34,33 @@ const members = {
     {'name': 'cornell University', 'id': 6}
   ]
 }
-global.fetch = sinon.stub().callsFake(() => Promise.resolve(members));
 
+
+//Mocks
+const sandbox = sinon.sandbox.create()
+
+const localStorageMock = {
+  getItem: sandbox.stub().callsFake(() => savedSearches),
+  setItem: sandbox.stub(),
+  clear: sandbox.stub()
+};
+global.localStorage = localStorageMock;
+
+global.fetch = sandbox.stub().callsFake(() => Promise.resolve({json: () => members}));
 
 const history = {
-  push: sinon.stub()
+  push: sandbox.stub()
 }
-const didMount = sinon.spy(Search.prototype, 'componentDidMount')
-const setState = sinon.spy(Search.prototype, 'setState')
+
+const didMount = sandbox.spy(Search.prototype, 'componentDidMount')
+const setState = sandbox.spy(Search.prototype, 'setState')
 
 
+
+
+
+//Render and begin tests
 const mountComponent = mount(<Search history={history}/>)
-
-
 
 
 
@@ -59,11 +73,11 @@ test('mount snapshot', () => {
 
 
 
-
 describe('componentDidMount', () => {
 
   test('should get savedSearches from localstorage', () => {
     expect(localStorageMock.getItem.callCount).toBe(1)
+    expect(localStorageMock.getItem.args[0][0]).toBe('savedSearches')
     expect(setState.args[0][0]).toEqual({savedSearches: parsedSavedSearches})
     expect(mountComponent.state('savedSearches')).toEqual(parsedSavedSearches)
   })
@@ -82,7 +96,7 @@ describe('componentDidMount', () => {
 describe('focus input', () => {
 
   test('onFocus event should set focused state to true', () => {
-    setState.resetHistory()
+    sandbox.resetHistory()
 
     mountComponent.find('input').prop('onFocus')()
     expect(setState.callCount).toBe(1)
@@ -103,7 +117,7 @@ describe('focus input', () => {
 
 
   test('onBlur event should make search results disappear', () => {
-    setState.resetHistory()
+    sandbox.resetHistory()
     mountComponent.find('input').prop('onBlur')()
     expect(setState.callCount).toBe(1)
     expect(mountComponent.state('focused')).toBe(false)
@@ -129,9 +143,91 @@ describe('focus input', () => {
 
 
 
-// describe('typing into search', () => {
-//
-//   test('...', () => {
-//     mountComponent.find('input').prop('onChange')
-//   })
-// })
+describe('searching', () => {
+  const filteredData = [
+    {'name': 'cambridge Core', 'id': 2},
+    {'name': 'cambridge University Press', 'id': 5}
+  ]
+  const selection = filteredData[0]
+
+
+  test('items array should be filtered by typed input, not case sensitive', () => {
+    sandbox.resetHistory()
+    mountComponent.find('Autocomplete').prop('onChange')(null, 'CAm')
+    expect(setState.callCount).toBe(1)
+    expect(mountComponent.state('searchingFor')).toBe('CAm')
+    mountComponent.update()
+
+    const searchResults = mountComponent.find('.searchInputHolder').children('div')
+    expect(searchResults.length).toBe(1)
+    expect(searchResults.prop('style').display).toBe('initial')
+
+    expect(mountComponent.find('Autocomplete').prop('items')).toEqual(filteredData)
+    expect(toJson(searchResults)).toMatchSnapshot()
+  })
+
+
+  test('if no search results, should not show results div, even if there are saved searches', () => {
+    mountComponent.setState({savedSearches: parsedSavedSearches})
+    mountComponent.update()
+
+    mountComponent.find('Autocomplete').prop('onChange')(null, 'xxxxxxx')
+    mountComponent.update()
+
+    const searchResults = mountComponent.find('.searchInputHolder').children('div')
+    expect(searchResults.length).toBe(1)
+    expect(searchResults.prop('style').display).toBe('none')
+  })
+
+
+  test('first selection should instantiate localStorage for savedSearches', () => {
+    sandbox.resetHistory()
+    savedSearches = null
+    mountComponent.find('Autocomplete').prop('onSelect')(selection.name, selection)
+    expect(localStorageMock.getItem.callCount).toBe(1)
+    expect(localStorageMock.setItem.callCount).toBe(1)
+    expect(localStorageMock.setItem.args[0]).toEqual(['savedSearches', JSON.stringify([selection])])
+  })
+
+
+  test('if search selection already saved, should move selection to front of list', () => {
+    sandbox.resetHistory()
+    savedSearches = JSON.stringify(parsedSavedSearches)
+    const repeatSelection = parsedSavedSearches[3]
+
+    mountComponent.find('Autocomplete').prop('onSelect')(repeatSelection.name, repeatSelection)
+
+    const newSavedSearches = [...parsedSavedSearches]
+    newSavedSearches.splice(3, 1)
+    newSavedSearches.unshift(repeatSelection)
+
+    expect(localStorageMock.setItem.args[0]).toEqual(['savedSearches', JSON.stringify(newSavedSearches)])
+  })
+
+
+  test('making a selection should save to localStorage and navigate to publisher page', () => {
+    sandbox.resetHistory()
+    mountComponent.find('Autocomplete').prop('onSelect')(selection.name, selection)
+
+    const newSavedSearches = [...parsedSavedSearches]
+    newSavedSearches.unshift(selection)
+    expect(localStorageMock.setItem.args[0]).toEqual(['savedSearches', JSON.stringify(newSavedSearches)])
+
+    expect(history.push.callCount).toBe(1)
+    expect(history.push.args[0][0]).toBe(`${deployConfig.baseUrl}cambridge Core/2`)
+  })
+
+
+  test('should only keep last 6 savedSearches', () => {
+    sandbox.resetHistory()
+    const savedSearchesArray= [...parsedSavedSearches, {name: 'saved search 26', id: 26}]
+    savedSearches = JSON.stringify(savedSearchesArray)
+
+    mountComponent.find('Autocomplete').prop('onSelect')(selection.name, selection)
+
+    savedSearchesArray.pop()
+    savedSearchesArray.unshift(selection)
+
+    expect(localStorageMock.setItem.args[0]).toEqual(['savedSearches', JSON.stringify(savedSearchesArray)])
+  })
+})
--- app/components/landingPage/components/search.js
+++ app/components/landingPage/components/search.js
@@ -21,15 +21,71 @@ export default class Search extends React.Component {
     savedSearches: undefined
   }
 
+
   componentDidMount () {
     this.setState({savedSearches: JSON.parse(localStorage.getItem('savedSearches'))})
-    fetch('https://www.crossref.org/prep/data?op=members')
+    fetch('https://apps.crossref.org/prep-staging/data?op=members')
+      .then( r => r.json())
       .then( r => this.setState({data: r.message}))
-      .catch(e=>console.error(e))
+      .catch(e=>{
+        console.error(e)
+      })
   }
 
 
+  onSelect = (value, selection) => {
+    let savedSearches = JSON.parse(localStorage.getItem('savedSearches'))
 
+    if(!savedSearches) {
+      savedSearches = [selection]
+
+    } else {
+      let savedIndex
+      const alreadySaved = savedSearches.some( (savedItem, i) => {
+        if(savedItem.id === selection.id && savedItem.name === selection.name) {
+          savedIndex = i
+          return true
+        }
+      })
+
+      if(alreadySaved) {
+        savedSearches.splice(savedIndex, 1)
+        savedSearches.unshift(selection)
+
+      } else {
+        if(savedSearches.length === 6) {
+          savedSearches.pop()
+        }
+        savedSearches.unshift(selection)
+      }
+    }
+
+    localStorage.setItem('savedSearches', JSON.stringify(savedSearches))
+    this.props.history.push(`${deployConfig.baseUrl}${selection.name}/${selection.id}`)
+  }
+
+
+  renderInput = (props) => {
+    return (
+      <input
+        {...props}
+        className={`searchInput ${this.state.focused ? 'searchFocused' : ''}`}
+        onFocus={()=>{
+          props.onFocus()
+          this.setState({focused: true})
+        }}
+        onBlur={()=>{
+          props.onBlur()
+          this.setState({focused: false})
+        }}
+        placeholder={`${this.state.focused ? '' : 'Search by member'}`}/>
+    )
+  }
+
+
+  renderItem = (item) => {
+    return <div className='searchItem' key={item.id}>{item.name}</div>
+  }
 
 
   render () {
@@ -37,22 +93,25 @@ export default class Search extends React.Component {
     if(!this.state.searchingFor && this.state.savedSearches) {
       data = this.state.savedSearches
     } else {
-      data = this.state.data.filter((item)=>item.name && item.name.includes(this.state.searchingFor))
+      data = this.state.data.filter((item)=>item.name && item.name.toLowerCase().includes(this.state.searchingFor.toLowerCase()))
       if(!data.length) {
-        data = ['no results found...']
+        data = []
       }
     }
 
     return (
       <Autocomplete
-        value={this.state.searchingFor}
+        renderInput={this.renderInput}
+        renderItem={(this.renderItem)}
         items={data}
-        onSelect={ result => this.props.history.push(`${deployConfig.baseUrl}${item.name}/${item.id}`)}
-        onChange={(e, value)=>this.setState({searchingFor: value})}
-        renderItem={(item)=><div className='searchItem' key={item.id}>{item.name}</div>}
-        getItemValue={ item => item }
+        getItemValue={ item => item.name }
+
+        value={this.state.searchingFor}
+        onChange={(e, value)=> this.setState({searchingFor: value})}
+        onSelect={this.onSelect}
+
         menuStyle={{
-          display: this.state.searchingFor || this.state.savedSearches ? 'initial' : 'none',
+          display: (this.state.searchingFor && data.length) || (!this.state.searchingFor && this.state.savedSearches) ? 'initial' : 'none',
           backgroundColor: 'white',
           padding: '13px 0 0 0',
           borderRadius: '0',
@@ -64,22 +123,7 @@ export default class Search extends React.Component {
           left: 'auto',
           border: '1px black solid'
         }}
-        renderInput={(props)=>{
-          return (
-            <input
-              {...props}
-              className={`searchInput ${this.state.focused ? 'searchFocused' : ''}`}
-              onFocus={()=>{
-                props.onFocus()
-                this.setState({focused: true})
-              }}
-              onBlur={()=>{
-                props.onBlur()
-                this.setState({focused: false})
-              }}
-              placeholder={`${this.state.focused ? '' : 'Search by member'}`}/>
-          )
-        }}
+
         wrapperProps={{
           className: 'searchInputHolder',
           style:{}
