14f3c0e9d9559e011d1ba5f9613c689f1e6ec928
leoandplus <lfabrikant@andplus.com>
2018-02-27 12:48:05 -0500

    PREP-15 checksSection filter test skeleton

---
+++ app/components/publisherPage/__tests__/checksSection.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import sinon from 'sinon'
+import ChecksSection from "../components/checksSection"
+
+
+
+
+
+//Dummy Data
+
+
+
+//Mocks
+const sandbox = sinon.sandbox.create()
+const setState = sandbox.spy(ContentTypeFilter.prototype, 'setState')
+
+
+
+
+//Render and begin tests
+const shallowComponent = shallow(<ChecksSection />)
+
+
+
+describe('filter is set to new value', () => {
+  test('new checks are rendered', () => {
+
+  })
+
+  test('titleBar filter type text updated', () => {
+
+  })
+})
