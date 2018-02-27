a056ae77b2faaca6a96aff9749cc10f16c59b476
leoandplus <lfabrikant@andplus.com>
2018-02-27 13:28:46 -0500

    PREP-29 shallow snapshot skeleton added to checksSection

---
+++ app/components/publisherPage/__tests__/checksSection.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import sinon from 'sinon'
+import ChecksSection from "../components/checksSection"
+
+
+
+
+
+//Dummy Data
+
+
+
+
+
+
+//Render and begin tests
+const shallowComponent = shallow(<ChecksSection />)
+
+
+
+test('shallow snapshot', () => {
+
+})
+
